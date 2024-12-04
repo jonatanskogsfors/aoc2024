1abf1505ffab77b8d1dbc0f845d09c8c6f80be3e
Jonatan Skogsfors <jonatan@skogsfors.net>
2024-12-04 07:37:34 +0100

    Day 04 **

---
+++ src/aoc2024/day_04.py
@@ -0,0 +1,90 @@
+import itertools
+from pathlib import Path
+
+
+def get_positions(given_word_search, letter):
+    positions = set()
+    for y, row in enumerate(given_word_search):
+        for x, position_letter in enumerate(row):
+            if position_letter == letter:
+                positions.add((x, y))
+    return positions
+
+
+def mas_candidates(x_position):
+    x, y = x_position
+    mas_candidates = set()
+    for x_delta, y_delta in itertools.product((-1, 0, +1), repeat=2):
+        if x_delta == 0 and y_delta == 0:
+            continue
+        mas_candidates.add(tuple((x + x_delta * n, y + y_delta * n) for n in range(1, 4)))
+    return mas_candidates
+
+
+def x_mas_candidates(a_position):
+    x, y = a_position
+    return {
+        ((x - 1, y - 1), (x + 1, y + 1)),
+        ((x - 1, y + 1), (x + 1, y - 1)),
+        ((x + 1, y - 1), (x - 1, y + 1)),
+        ((x + 1, y + 1), (x - 1, y - 1)),
+    }
+
+
+def is_mas(wordsearch, mas_candidate):
+    for letter, (x, y) in zip("MAS", mas_candidate):
+        if not (0 <= y < len(wordsearch) and 0 <= x < len(wordsearch[0])):
+            return False
+        if wordsearch[y][x] != letter:
+            return False
+    return True
+
+
+def is_x_mas(wordsearch, x_mas_candidate):
+    for letter, (x, y) in zip("MS", x_mas_candidate):
+        if not (0 <= y < len(wordsearch) and 0 <= x < len(wordsearch[0])):
+            return False
+        if wordsearch[y][x] != letter:
+            return False
+    return True
+
+
+def parse_input(input_path: Path):
+    return tuple(input_path.read_text().strip().split("\n"))
+
+
+def solve_part_one(input_path: Path):
+    wordsearch = parse_input(input_path)
+    x_positions = get_positions(wordsearch, "X")
+    return len(
+        [
+            candidate
+            for x in x_positions
+            for candidate in mas_candidates(x)
+            if is_mas(wordsearch, candidate)
+        ]
+    )
+
+
+def solve_part_two(input_path: Path):
+    wordsearch = parse_input(input_path)
+    a_positions = get_positions(wordsearch, "A")
+    return len(
+        [
+            a_position
+            for a_position in a_positions
+            if len(
+                [
+                    candidate
+                    for candidate in x_mas_candidates(a_position)
+                    if is_x_mas(wordsearch, candidate)
+                ]
+            )
+            > 1
+        ]
+    )
+
+
+if __name__ == "__main__":
+    print(solve_part_one(Path("input/input_04.txt")))
+    print(solve_part_two(Path("input/input_04.txt")))
